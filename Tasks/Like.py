1d68c51e5f3dbb9f5f3ced54e8df37030e5a814d
Sergei Ruzki <sergei.ruzki@icode.by>
2020-06-25 20:26:20 +0300

    init

---
+++ Tasks/Like.py
@@ -0,0 +1,35 @@
+'''
+Мне нравится 👍
+Создайте функцию, которая, принимая массив имён, возвращает строку описывающая количество лайков (как в Facebook).
+
+def likes(*arr: str) -> str:
+    pass
+Примеры:
+
+likes() -> "no one likes this"
+likes("Peter") -> "Peter likes this"
+likes("Jacob", "Alex") -> "Jacob and Alex like this"
+likes("Max", "John", "Mark") -> "Max, John and Mark like this"
+likes("Alex", "Jacob", "Mark", "Max") -> "Alex, Jacob and 2 others like this"
+Бонусные очки
+Функция работает на нескольких языках и кодировках - язык ответа определяется по языку входного массива.
+'''
+
+
+class MyClass:
+
+    def likes(self, var: str) -> str:
+        result = ''
+
+        return result
+
+
+
+if __name__ == '__main__':
+    # Here we can make console input and check how function works
+
+    var = input('Input names: ')
+
+    result = MyClass().likes(var)
+
+    print(result)
---
+++ Tasks/TypingDecorator.py
@@ -0,0 +1,62 @@
+'''
+Декоратор типов
+Напишите декоратор, который проверял бы тип параметров функции следующим образом: При вызове без аргументов осуществлял бы конвертацию параметров и возвращаемого значения в указанные типы:
+
+@typed
+def add(a: int, b: int) -> str:
+    return a + b
+
+add("3", 5) -> "8"
+add(2.35, True) -> "3"
+При вызове с параметром strict=True выбрасывал бы исключение при неправильной передаче параметров:
+
+@typed(strict=True)
+def convert_upper(msg: str) -> str:
+    return msg.upper()
+
+convert_upper('abc') -> 'ABC'
+convert_upper(5) -> TypeError('`convert_upper` argument `msg` required to be a `str` instance')
+Если типы параметров функции не указаны декоратор должен предполагать их тип как Any:
+
+@typed
+def acc(a, b, c):
+    return a + b + c
+
+acc('a', 'b', 'c') -> 'abc'
+acc(5, 6, 7) -> 18
+acc(0.1, 0.2, 0.4) -> 0.7000000000000001
+'''
+
+
+class MyClass:
+
+    @typed
+    def add(a: int, b: int) -> str:
+        return a + b
+
+    @typed(strict=True)
+    def convert_upper(msg: str) -> str:
+        return msg.upper()
+
+    @typed
+    def acc(a, b, c):
+        return a + b + c
+
+if __name__ == '__main__':
+    # Here we can make console input and check how function works
+
+    a =
+    b =
+
+    result = MyClass().add(a, b)
+    print(result)
+
+    msg =
+    result = MyClass().convert_upper(msg)
+    print(result)
+
+    a =
+    b =
+    c =
+    result = MyClass().convert_upper(a, b, c)
+    print(result)
