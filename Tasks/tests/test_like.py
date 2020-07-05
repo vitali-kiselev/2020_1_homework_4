04143f600c63d4a38759375f47e1cf14165e3697
Виталий Киселев <v>
2020-07-05 11:20:57 +0300

    homework

--- Tasks/tests/test_like.py
+++ Tasks/tests/test_like.py
@@ -2,6 +2,10 @@ import unittest
 from coursepy.HW4.Tasks import Like
 
 class LikeTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
     def test_like(self):
         self.assertEqual()
 
